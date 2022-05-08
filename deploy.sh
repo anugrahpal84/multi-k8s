c83671129d01e0c1c87a765254283b259e1e50c3
anugrah pal <anugrahpal@gmail.com>
2022-05-08 12:25:07 +0530

    modified travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t anugrahpal84/multi-client:latest -t anugrahpal84/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t anugrahpal84/multi-server:latest -t anugrahpal84/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t anugrahpal84/multi-worker:latest -t anugrahpal84/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t anugrahpal/multi-client:latest -t anugrahpal/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t anugrahpal/multi-server:latest -t anugrahpal/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t anugrahpal/multi-worker:latest -t anugrahpal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push anugrahpal84/multi-client:latest
-docker push anugrahpal84/multi-server:latest
-docker push anugrahpal84/multi-worker:latest
+docker push anugrahpal/multi-client:latest
+docker push anugrahpal/multi-server:latest
+docker push anugrahpal/multi-worker:latest
 
-docker push anugrahpal84/multi-client:$SHA
-docker push anugrahpal84/multi-server:$SHA
-docker push anugrahpal84/multi-worker:$SHA
+docker push anugrahpal/multi-client:$SHA
+docker push anugrahpal/multi-server:$SHA
+docker push anugrahpal/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=anugrahpal84/multi-server:$SHA
-kubectl set image deployments/client-deployment client=anugrahpal84/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=anugrahpal84/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=anugrahpal/multi-server:$SHA
+kubectl set image deployments/client-deployment client=anugrahpal/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=anugrahpal/multi-worker:$SHA
